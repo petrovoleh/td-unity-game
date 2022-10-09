7d6195d461907b904fc8c6dbc2d970baff2d008c
Petrov Oleh <81036385+petrovoleh@users.noreply.github.com>
2022-10-09 13:07:14 +0300

    added selecting different towers
    Former-commit-id: 191214a781b2c25b331998c5c32b7ee02a293f29

---
+++ src/v 0.0/Assets/Scripts/DeleteSelection.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteSelection : MonoBehaviour
+{
+    bool not_in_object = false;
+    void Update()
+    {
+        if ((Input.GetMouseButtonDown(0)) && (gameObject.name != "Selected") && not_in_object){
+            Destroy(gameObject);
+        }
+    }
+    void OnMouseExit()
+    {
+        not_in_object=true;
+        
+    }
+}
